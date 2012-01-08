a48a5ad77bdcf17dd876ddbf7fdb0f13ad272db0
Lucas Dickey <lucas@lucasdickey.com>
2012-01-07 21:48:52 -0800

    Adding a some new layout files and a new adapter.

--- gen/com/TimeTracker/R.java
+++ gen/com/TimeTracker/R.java
@@ -15,12 +15,16 @@ public final class R {
     }
     public static final class id {
         public static final int counter=0x7f050000;
+        public static final int lap_name=0x7f050005;
+        public static final int lap_time=0x7f050006;
         public static final int reset=0x7f050002;
         public static final int start_stop=0x7f050001;
         public static final int time_list=0x7f050003;
+        public static final int time_row=0x7f050004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int time_row=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
@@ -28,5 +32,6 @@ public final class R {
         public static final int reset=0x7f040002;
         public static final int start=0x7f040003;
         public static final int stop=0x7f040004;
+        public static final int task_name=0x7f040005;
     }
 }
